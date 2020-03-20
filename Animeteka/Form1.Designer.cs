d27882db1793b572e3c543bd12a8e457850a5890
AdMyNyRaL AdMyNyRaL <jion93@mail.ru>
2020-03-20 15:04:35 +0200

    Search Filter Demo

--- Animeteka/Form1.Designer.cs
+++ Animeteka/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.atype = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -51,11 +52,19 @@
             this.textBox1.Size = new System.Drawing.Size(734, 345);
             this.textBox1.TabIndex = 1;
             // 
+            // atype
+            // 
+            this.atype.Location = new System.Drawing.Point(395, 62);
+            this.atype.Name = "atype";
+            this.atype.Size = new System.Drawing.Size(100, 20);
+            this.atype.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.atype);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -70,6 +79,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox atype;
     }
 }
 
