1aed95d080f9195d9f62ee76a188ac93bf4129ae
Bejenar <admynyral2808@gmail.com>
2020-03-21 18:12:02 +0200

    start with WelcomePage

--- Animeteka/Program.cs
+++ Animeteka/Program.cs
@@ -19,7 +19,7 @@ namespace Animeteka
             db = new AnimetekaContext();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new WelcomePage());
         }
     }
 }
